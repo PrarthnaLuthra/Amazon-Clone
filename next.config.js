eaebe433cadf17cb0cb8ab7fcf24a9f632f50820
Prarthna <prarthnaluthra@gmail.com>
2022-04-12 23:59:57 +0530

    Amazon Clone- Initial Commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// const withImages = require("next-images");
+module.exports = {
+  images: {
+    domains: ["fakestoreapi.com"],
+  },
+};
+// module.exports = withImages();
---
+++ src/pages/index.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Header from "../components/header";
+import Banner from "../components/banner";
+import ProductFeed from '../components/ProductFeed'
+import Footer from "../components/footer";
+export default function Home({products}) {
+  return (
+    <div className="bg-gray-100"> 
+      <Head>
+        <title>Amazon Clone</title>
+      </Head>
+      <Header/>
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner/>
+        <ProductFeed products={products}/>
+        
+
+
+      </main>
+      
+        <Footer/>
+      
+    </div>
+  );
+}
+// Server side rendering
+
+export async function getServerSideProps(context){
+   const products =await fetch("https://fakestoreapi.com/products").then(
+     (res)=>res.json());
+     return{
+       props:{
+         products,
+       }
+     }
+}
+//GET https://fakestoreapi.com/products
